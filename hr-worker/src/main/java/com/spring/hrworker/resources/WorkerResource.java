d34c016cfc3351194fb9aeb92e262b950948dfbe
Thallys Cezar <thalys_cezar@hotmail.com>
2023-10-26 10:27:22 -0300

    refactor: hr-worker as a config server client

--- hr-worker/src/main/java/com/spring/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/spring/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,9 @@ import com.spring.hrworker.services.WorkerService;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
@@ -37,5 +41,11 @@ public class WorkerResource {
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		return ResponseEntity.ok(service.findById(id));
+	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
 	}
 }
